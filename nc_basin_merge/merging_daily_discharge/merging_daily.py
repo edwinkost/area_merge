a406b163166a5d97ab8ab0a0b62b01addca8a1db
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-02-24 14:23:19 +0100

    Fixing.

--- nc_basin_merge/merging_daily_discharge/merging_daily.py
+++ nc_basin_merge/merging_daily_discharge/merging_daily.py
@@ -13,11 +13,9 @@ output_folder    = '/scratch-shared/edwinhs/tmp/daily_file/'
 
 # interval for years:
 year_int  = [1986, 1987]
-last_year = 1988
 
 # input folders (the sequence must be consistent with the list 'year_int'
 input_folder = [
-'/projects/0/wtrcycle/users/edwin/05min_runs_january_2016/pcrglobwb_only_from_1958_4LCs_edwin_parameter_set_kinematic_wave/continue_from_1983',
 '/projects/0/wtrcycle/users/edwin/05min_runs_january_2016/pcrglobwb_only_from_1958_4LCs_edwin_parameter_set_kinematic_wave/continue_from_1983',
 '/projects/0/wtrcycle/users/edwin/05min_runs_january_2016/pcrglobwb_only_from_1958_4LCs_edwin_parameter_set_kinematic_wave/continue_from_1983',
 ]
@@ -35,7 +33,6 @@ os.system(cmd)
 cmd = ''; print cmd
 for i_year in range(0, len(year_int)-1):
     cmd += 'python nc_basin_merge.py %s %s/%i_to_%i/ %i %i-01-01 %i-12-31 selected %s & ' %(input_folder[i_year]         , output_folder, year_int[i_year]         , year_int[i_year+1]-1, num_of_cores, year_int[i_year]         , year_int[i_year+1]-1, netcdf_file_name)
-cmd     += 'python nc_basin_merge.py %s %s/%i_to_%i/ %i %i-01-01 %i-12-31 selected %s & ' %(input_folder[len(year_int)-1], output_folder, year_int[len(year_int)-1], last_year,            num_of_cores, year_int[len(year_int)-1], last_year           , netcdf_file_name)
 cmd     += 'wait'
 print cmd
 os.system(cmd)
