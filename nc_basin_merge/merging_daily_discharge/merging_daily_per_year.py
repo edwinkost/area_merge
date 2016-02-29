22d0504022cd982a6e0e5268b91b9f68e219b019
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-02-29 18:28:16 +0100

    Test merging daily discharge.

--- nc_basin_merge/merging_daily_discharge/merging_daily_per_year.py
+++ nc_basin_merge/merging_daily_discharge/merging_daily_per_year.py
@@ -5,26 +5,27 @@ import os
 import sys
 import glob
 
-# input file name (the sequence must be consistent with the list 'year_int'
-input_file_name  = sys.argv[1]
+# input folder
+input_folder  = sys.argv[1]
+
+# netcdf file name
+netcdf_file_name = os.path.basename(sys.argv[2])                                                   # example: netcdf_file_name = "test0.test1.test2.nc"
+netcdf_file_name_splitted = netcdf_file_name.split(".")
+netcdf_file_name_without_extension = ""
+for i in range(0, len(netcdf_file_name_splitted) - 1, 1): 
+    netcdf_file_name_without_extension += netcdf_file_name_splitted[i]
+    if (i < (len(netcdf_file_name_splitted) - 2)): netcdf_file_name_without_extension += "."
 
 # interval for years:
-start_year   = as.integer(sys.argv[2])
-end_year     = as.integer(sys.argv[3])
+start_year   = as.integer(sys.argv[3])
+end_year     = as.integer(sys.argv[4])
 years = range(start_year, end_year + 1)
 
 # output folder
-output_folder = sys.argv[2]
+output_folder = sys.argv[4]
 
 # maximum number of cores used per command lines
-num_of_cores = as.integer(sys.argv[4])
-
-# netcdf file name
-netcdf_file_name = os.path.basename(sys.argv[1]) 
-netcdf_file_name_splitted = os.path.split(netcdf_file_name)
-netcdf_file_name_without_extension = ""
-for i in 0:length(netcdf_file_name_splitted):
-    netcdf_file_name_without_extension += netcdf_file_name_splitted[i]
+num_of_cores = as.integer(sys.argv[5])
 
 # preparing output folder
 cmd = 'mkdir '+str(output_folder)
@@ -33,7 +34,7 @@ os.system(cmd)
 # merging daily resolution over areas
 cmd = ''; print cmd
 for year in years:
-    cmd += 'python nc_basin_merge_daily.py ' + str(input_file_name) + " " + \
+    cmd += 'python nc_basin_merge_daily.py ' + str(input_folder) + " " + \
                                                str(output_folder) + "/" + netcdf_file_name_without_extension + "_" +  str(year) + ".nc" + " " + \
                                                "1" + " " + \
                                                str(year) + "-01-01 " + str(year) + "-12-31 " + \
