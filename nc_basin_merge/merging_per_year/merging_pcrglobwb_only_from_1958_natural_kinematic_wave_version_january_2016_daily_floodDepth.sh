1c2b91f2e209802be4f8c608e16f6fda54402666
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2016-03-04 18:10:19 +0100

    merging_pcrglobwb_only_from_1958_natural_kinematic_wave_version_january_2016 - several daily values

---
+++ nc_basin_merge/merging_per_year/merging_pcrglobwb_only_from_1958_natural_kinematic_wave_version_january_2016_daily_floodDepth.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 5:00:00 
+#SBATCH -p fat                                                                                                                                                                              
+
+# floodDepth
+python merging_per_year.py /projects/0/wtrcycle/users/edwin/05min_runs_january_2016/pcrglobwb_only_from_1958_natural_kinematic_wave/continue_from_1958 floodDepth_dailyTot_output.nc daily 1958 1965 /scratch-shared/edwin/05min_runs_january_2016_merged/pcrglobwb_only_from_1958_natural_kinematic_wave/daily/ &
+python merging_per_year.py /projects/0/wtrcycle/users/edwin/05min_runs_january_2016/pcrglobwb_only_from_1958_natural_kinematic_wave/continue_from_1966 floodDepth_dailyTot_output.nc daily 1966 1971 /scratch-shared/edwin/05min_runs_january_2016_merged/pcrglobwb_only_from_1958_natural_kinematic_wave/daily/ &
+python merging_per_year.py /projects/0/wtrcycle/users/edwin/05min_runs_january_2016/pcrglobwb_only_from_1958_natural_kinematic_wave/continue_from_1972 floodDepth_dailyTot_output.nc daily 1972 1978 /scratch-shared/edwin/05min_runs_january_2016_merged/pcrglobwb_only_from_1958_natural_kinematic_wave/daily/ &
+python merging_per_year.py /projects/0/wtrcycle/users/edwin/05min_runs_january_2016/pcrglobwb_only_from_1958_natural_kinematic_wave/continue_from_1979 floodDepth_dailyTot_output.nc daily 1979 1992 /scratch-shared/edwin/05min_runs_january_2016_merged/pcrglobwb_only_from_1958_natural_kinematic_wave/daily/ &
+python merging_per_year.py /projects/0/wtrcycle/users/edwin/05min_runs_january_2016/pcrglobwb_only_from_1958_natural_kinematic_wave/continue_from_1993 floodDepth_dailyTot_output.nc daily 1993 1999 /scratch-shared/edwin/05min_runs_january_2016_merged/pcrglobwb_only_from_1958_natural_kinematic_wave/daily/ &
+python merging_per_year.py /projects/0/wtrcycle/users/edwin/05min_runs_january_2016/pcrglobwb_only_from_1958_natural_kinematic_wave/continue_from_2000 floodDepth_dailyTot_output.nc daily 2000 2010 /scratch-shared/edwin/05min_runs_january_2016_merged/pcrglobwb_only_from_1958_natural_kinematic_wave/daily/ &
+wait
